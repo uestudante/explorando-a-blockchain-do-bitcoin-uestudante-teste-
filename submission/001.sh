0927a97a17f8d58b76c5a625f5ba25105a7f9f5e
uestudante <40926073+uestudante@users.noreply.github.com>
2024-11-15 18:28:19 -0300

    Update 001.sh
    2nd change

--- submission/001.sh
+++ submission/001.sh
@@ -1,2 +1,3 @@
 # What is the hash of block 654,321?
-bitcoin-cli -rcpconnect=84.247.182.145 -rcpuser=user_266 -rcppassoword=KvV48aXGz26Y getblockhash 654321
+#!/bin/sh
+bitcoin-cli getblockhash 654321
